2695db72340b4972968450e93d04bfb8af1544ef
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2019-04-01 20:13:00 -0500

    .gitignore added

--- Jebhave/src/main/java/com/san/Jbehave/App.java
+++ Jebhave/src/main/java/com/san/Jbehave/App.java
@@ -9,6 +9,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
-        System.out.println("Git Sync");
+        System.out.println("Git Sync with Change");
     }
 }
