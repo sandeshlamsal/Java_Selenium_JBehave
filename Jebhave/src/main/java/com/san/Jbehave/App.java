474019799f65f4f7352084c7e7878b7a97c81ced
DESKTOP-SHJRQ9O\Puspanjali <sunwalsandesh@gmail.com>
2019-04-16 22:22:55 -0500

    for server

--- Jebhave/src/main/java/com/san/Jbehave/App.java
+++ Jebhave/src/main/java/com/san/Jbehave/App.java
@@ -1,15 +1,11 @@
 package com.san.Jbehave;
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "1st Line" );
         System.out.println("2nd Line");
-        System.out.println("3rd Line 1");
+        System.out.println("Pushed to Server");
     }
 }
